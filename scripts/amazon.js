340438857b739ff648a6259c214de4613c8c30e3
Rachel <hurui.lcy@gmail.com>
2024-07-24 17:49:43 -0600

    Use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,32 +1,6 @@
-const products = [{
-  image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-
-}, {
-  image: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  rating: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-}, {
-  image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-}];
+import { cart } from '../data/cart.js';
 
 let productHTML = ``;
-
 
 products.forEach((product) => {
   productHTML = productHTML + `
@@ -72,7 +46,9 @@ products.forEach((product) => {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button 
+        button-primary js-add-to-cart"
+        data-product-id = "${product.id}" >
           Add to Cart
         </button>
      </div>
@@ -85,3 +61,40 @@ console.log(productHTML);
 
 document.querySelector('.js-products-grid')
   .innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1
+        });
+      };
+
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+      console.log(cartQuantity);
+      console.log(cart);
+    });
+  });
+
