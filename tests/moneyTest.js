b794128504a93fedc2a3de27d4547c60617625e4
Rachel <hurui.lcy@gmail.com>
2024-08-02 19:08:05 -0600

    Add jasmine testing framework

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -1,7 +1,8 @@
 import { formatCurrency } from '../scripts/utils/money.js';
 
-console.log('Hello');
+console.log('test suite: formatCurrency');
 
+console.log('coverts cents into dollars');
 if (formatCurrency(2095) === '20.95') {
   console.log('passed');
 } else {
@@ -9,7 +10,8 @@ if (formatCurrency(2095) === '20.95') {
 }
 
 
+console.log('works with 0');
 if (formatCurrency(0) === '0.00') {
   console.log('passed');
 }
-console.log('Hello');
+
