e5535e36e32408436db85da7ec5a4c192fa95613
Rachel <hurui.lcy@gmail.com>
2024-08-07 19:25:21 -0600

    remove default test and add moneyTest

--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -15,3 +15,9 @@ if (formatCurrency(0) === '0.00') {
   console.log('passed');
 }
 
+console.log('rounds up to the nearest cent');
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+} else {
+  console.log('failed');
+}
