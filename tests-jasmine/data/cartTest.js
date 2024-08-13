7e51a2b4f5c7b6b97f39461fb7a88480db3bc4f6
Rachel <hurui.lcy@gmail.com>
2024-08-12 19:30:33 -0600

    create automated tests for orderSummary

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -3,6 +3,7 @@ describe('test suit: addToCart', () => {
   it('adds an existing product to the cart', () => {
 
     spyOn(localStorage, 'setItem');
+
     spyOn(localStorage, 'getItem').and.callFake(() => {
       return JSON.stringify([{
         productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
