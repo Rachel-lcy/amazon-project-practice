79dc85f1ac9cbe3008f4e25c6f586837d83402ad
Rachel <hurui.lcy@gmail.com>
2024-08-22 15:52:43 -0600

    create order using the backend

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -3,6 +3,8 @@ import { getProduct } from '../../data/products.js';
 import { getDeliveryOption } from '../../data/deliveryOptions.js';
 import { formatCurrency } from '../utils/money.js';
 
+import { addOrder } from '../../data/order.js';
+
 export function renderPaymentSummary() {
   let productPriceCents = 0;
   let shippingPriceCents = 0;
@@ -56,12 +58,34 @@ export function renderPaymentSummary() {
             $${formatCurrency(totalCents)}</div>
         </div>
 
-        <button class="place-order-button button-primary">
+        <button class="place-order-button button-primary js-place-order">
           Place your order
         </button>
   `;
   document.querySelector('.js-payment-summary')
     .innerHTML = paymentSummaryHTML;
+
+  document.querySelector('.js-place-order')
+    .addEventListener('click', async () => {
+      try {
+        const response = await fetch('https://supersimplebackend.dev/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            cart: cart
+          })
+        });
+        const order = await response.json();
+        addOrder(order);
+      } catch (error) {
+        console.log('unexpected error. Try again later.')
+      }
+      window.location.href = 'orders.html';
+    });
 }
 
 
+
+localStorage.removeItem('orders')
